56fd18722898ce924f73290db0dfc2d57b4fa3f5
Karim Wölbert <karimwolbert@MacBook-Pro.fritz.box>
2022-01-14 17:56:29 +0100

    added day1

---
+++ main.py
@@ -0,0 +1,7 @@
+#Day 1: Creating Band Name Generator
+print("Welcome to the Band Name Generator.")
+cityname = input("What's name of the city you grew up in? \n")
+petsname = input("What's your pet's name? \n")
+print("Your band name could be " + cityname + " " + petsname)
+
+
